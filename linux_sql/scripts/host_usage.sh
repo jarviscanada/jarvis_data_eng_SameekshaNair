a80a02268c319bbf9b42203cdbfce13088093ba2
Sameeksha Nair <sameekshanr@gmail.com>
2023-12-04 22:12:26 +0000

    fixed some issues

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -18,13 +18,14 @@ memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
 cpu_idle=$(echo "$vmstat_mb" | awk '{print $(NF-2)}'| tail -n1 | xargs)
 cpu_kernel=$(echo "$vmstat_mb" | awk '{print $(NF-3)}'| tail -n1 | xargs)
 disk_io=$(echo "$vmstat_mb" | awk '{print $10}' | tail -n1 | xargs)
-disk_available=$(df -BM / | awk '{print $4}' | tail -n1 | xargs)
+disk_available=$(df -BM / | awk '{print $4}' | awk '{print substr($0, 1, 5)};' | tail -n1 | xargs)
 
 timestamp=$(vmstat -t | awk '{print $(NF-1), $NF}' | tail -n1 | xargs)
 
-host_id="SELECT id FROM host_info WHERE hostname='$hostname'";
+host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
 insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
-VALUES('$timestamp', '$host_id', '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', '$disk_available');"
+VALUES('$timestamp', $host_id, '$memory_free', '$cpu_idle', '$cpu_kernel', '$disk_io', $disk_available);
+SELECT * from host_usage;"
 
 export PGPASSWORD=$psql_password
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
